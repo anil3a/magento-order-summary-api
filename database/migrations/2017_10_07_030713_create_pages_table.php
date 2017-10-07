4039b9bd5f8f898e633d20b604cb9a5ee1392ce0
anil3a <anilprz3@gmail.com>
2017-10-07 14:18:41 +1100

    First commit for api

---
+++ database/migrations/2017_10_07_030713_create_pages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funnelpage', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', '100');
+            $table->string('url', '500')->nullable();
+            $table->boolean('active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funnelpage');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Magento Order API Alternate Storing data
+
+
+## Contributors
+
+Please send me a pull request if you want to extend this module and have crazy thoughts.
+
+## License
+
+GPL
