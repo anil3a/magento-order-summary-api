6d7e4fa4a368abc2ade15d7745c991f8409cfce2
anil3a <anilprz3@gmail.com>
2017-10-09 20:15:49 +1100

    revert header in middleware

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -15,18 +15,6 @@ class BasicAuthMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
-        ];
-
-        if ($request->isMethod('OPTIONS'))
-        {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
         if( $request->getUser() != env('API_USERNAME') || 
             $request->getPassword() != env('API_PASSWORD') 
         ){
@@ -34,13 +22,7 @@ class BasicAuthMiddleware
             return response('Unauthorized', 401, $headers);
         }
         
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
-
-        return $response;
+        return $next($request);
     }
 
 }
